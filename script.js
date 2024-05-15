5d11d6feca81737e8e482b00876cb072863f4abc
John M. Bauer <161013370+jmbauer3@users.noreply.github.com>
2024-05-15 11:03:31 +0200

    fdszsa

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 window.onload = function() {
     const music = document.getElementById('backgroundMusic');
+    const startButton = document.getElementById('start-button');
+    const mainMenu = document.getElementById('main-menu');
+
     music.volume = 0.2;
     music.play();
 
-    const startButton = document.getElementById('start-button');
     startButton.addEventListener('click', () => {
-        alert('Game started!');
+        mainMenu.style.display = 'none';
+        document.body.style.backgroundColor = 'black';
     });
 }
